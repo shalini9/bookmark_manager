4f6022fe3bca0c05518077b39c9c5bb7ea3e7786
shalini9 <shalini.nangia1@gmail.com>
2022-02-07 15:02:30 +0000

    viewing bookmark links

--- app.rb
+++ app.rb
@@ -10,5 +10,16 @@ class BookmarkManager < Sinatra::Base
     'Bookmark Manager'
   end
 
+  get '/bookmarks' do
+    bookmarks = [
+              "http://www.makersacademy.com",
+              "http://www.destroyallsoftware.com",
+              "http://www.google.com"
+             ]
+  
+    bookmarks.join
+  end
+
+
   run! if app_file == $0
 end
