8cfbdb07f17485bab17f4f08299d5767e2f9b611
shalini9 <shalini.nangia1@gmail.com>
2022-02-10 10:17:46 +0000

    fixed errors, wrapped setup test database in method

--- spec/setup_test_database.rb
+++ spec/setup_test_database.rb
@@ -1,9 +1,7 @@
-
 require 'pg'
-
-p "Setting up test database..."
-
-connection = PG.connect(dbname: 'bookmark_manager_test')
-
-# Clear the bookmarks table
-connection.exec("TRUNCATE bookmarks;")
+def setup_test_database
+  # p "Setting up test database..."
+  connection = PG.connect(dbname: 'bookmark_manager_test')
+  # Clear the bookmarks table
+  connection.exec("TRUNCATE bookmarks;")
+end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,4 +1,17 @@
 require_relative './setup_test_database'
+# Set the environment to "test"
+ENV['RACK_ENV'] = 'test'
+
+# Bring in the contents of the `app.rb` file. The below is equivalent to: require_relative '../app.rb'
+require File.join(File.dirname(__FILE__), '..', 'app.rb')
+
+# Require all the testing gems
+require 'capybara'
+require 'capybara/rspec'
+require 'rspec'
+
+# Tell Capybara to talk to BookmarkManager
+Capybara.app = BookmarkManager
 
 ENV['ENVIRONMENT'] = 'test'
 
@@ -107,21 +120,7 @@ RSpec.configure do |config|
 
 # at the top of spec/spec_helper.rb
 
-# Set the environment to "test"
-ENV['RACK_ENV'] = 'test'
 
-# Bring in the contents of the `app.rb` file. The below is equivalent to: require_relative '../app.rb'
-require File.join(File.dirname(__FILE__), '..', 'app.rb')
-
-# Require all the testing gems
-require 'capybara'
-require 'capybara/rspec'
-require 'rspec'
-
-
-
-# Tell Capybara to talk to BookmarkManager
-Capybara.app = BookmarkManager
 
 
 ### the rest of the file ###
